d258bab833bfc73f7be3e9c9a89b77c334b658fa
Jemojem <rexitorchanel@yandex.ru>
2024-09-21 19:15:55 +0300

    sd

---
+++ strweb2part2/src/main/java/com/strweb2/strweb2part2/Strweb2part2Application.java
@@ -0,0 +1,10 @@
+package com.strweb2.strweb2part2;
+
+
+public class Strweb2part2Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ strweb2part2/src/test/java/com/strweb2/strweb2part2/Strweb2part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strweb2.strweb2part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Strweb2part2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
